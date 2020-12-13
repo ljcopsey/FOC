2201e04eacf2321c8d6b0419152243b0be23448b
Libby Copsey <libbycopsey@hotmail.co.uk>
2020-12-13 19:04:06 +0000

    generateParseTree using derivations. All tests failing.

--- Parser.java
+++ Parser.java
@@ -10,49 +10,59 @@ import java.util.List;
 
 public class Parser implements IParser {
     public boolean isInLanguage(ContextFreeGrammar cfg, Word w) {
-        List<Word> activeStrings = new ArrayList<>();
+        return false;
+    }
+
+    public ParseTreeNode generateParseTree(ContextFreeGrammar cfg, Word w) {
+        List<Derivation> activeDerivations = new ArrayList<>();
         List<Rule> rules;
         rules = cfg.getRules();
 
         Variable startVariable = cfg.getStartVariable();
         Word startWord = new Word(startVariable);
-        activeStrings.add(startWord);
+        Derivation derivations = new Derivation(startWord);
+        activeDerivations.add(derivations);
 
-//    Variable S = MyGrammar.courseworkCNF().getStartVariable();
-//    Word start = new Word(S);
-//    activeStrings.add(start);
+        List<ParseTreeNode> children = new ArrayList<>();
+        ParseTreeNode treeNode;
 
         int steps = 0;
         int n = w.length();
         while (steps < (2 * n - 1)) {
-            List<Word> newActiveStrings = new ArrayList<>();
-            for (Word activeString : activeStrings) {
-                for (int i = 0; i < activeString.length(); i++) {
-                    Symbol symbol = activeString.get(i);
+            List<Derivation> newActiveDerivations = new ArrayList<>();
+            for (Derivation activeDerivation : activeDerivations) {
+                for (int i = 0; i < activeDerivation.getLatestWord().length(); i++) {
+                    Symbol symbol = activeDerivation.getLatestWord().get(i);
                     for (Rule rule : rules) {
                         Variable ruleVariable = rule.getVariable();
                         if (symbol.equals(ruleVariable)) {
                             Word expansion = rule.getExpansion();
-                            Word replace = activeString.replace(i, expansion);
-                            newActiveStrings.add(replace);
+                            Word replaceWord = activeDerivation.getLatestWord().replace(i, expansion);
+                            Derivation replaceDerivation = new Derivation(activeDerivation);
+                            replaceDerivation.addStep(replaceWord, rule, i);
+                            newActiveDerivations.add(replaceDerivation);
                         }
                     }
                 }
             }
-            activeStrings = newActiveStrings;
+            activeDerivations = newActiveDerivations;
             steps++;
-        }
-        System.out.println(activeStrings);
-        return activeStrings.contains(w);
-    }
 
-    public ParseTreeNode generateParseTree(ContextFreeGrammar cfg, Word w) {
-        if (this.isInLanguage(cfg, w)) {
-            //generate parse tree
-            generateParseTree(cfg, w);
-        } else {
-            System.out.println("Word is not in grammar");
         }
-        return null;
+        if (activeDerivations.contains(w)) {
+            for (Step derivation : derivations) {
+                if (derivation.getRule().getExpansion().isTerminal()) {
+                    do {
+                        int i = derivation.getIndex();
+                        Rule rule = derivation.getRule();
+                        Symbol symbol = rule.getVariable();
+                        ParseTreeNode parseTreeNode = new ParseTreeNode(symbol);
+                        children.add(parseTreeNode);
+                    }
+                    while (!derivation.isStartSymbol());
+                }
+            }
+        }
+        return ParseTreeNode.emptyParseTree(startVariable);
     }
 }
